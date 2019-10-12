baa3b69f17335b94705d228e2f677db4ae0c15e6
seantyh <seantyh@gmail.com>
2019-10-13 02:30:56 +0800

    update segmenter

--- gran/segment/segmenter.py
+++ gran/segment/segmenter.py
@@ -1,13 +1,16 @@
 from ..utils import *
 import jieba
 from .lexicon import Lexicon
+import logging
 
 class Segmenter:
     def __init__(self, lexicon):
         self.lexicon: Lexicon = lexicon
+        self.logger = logging.getLogger("Segmenter")
+        self.logger.setLevel("DEBUG")
 
     def preprocess_matches(self, matches):
-        matches = sorted(matches, key=lambda x: (x[0], -x[1]))
+        matches = sorted(matches, key=lambda x: (-x[1], x[0]))
         start_idx_set = []
         preproc_mat = []
         for start_idx, nchar in matches:
@@ -19,8 +22,59 @@ class Segmenter:
                 preproc_mat.append((start_idx, nchar))
         return preproc_mat
 
+    def find_best_route(self, matches):        
+        best_route = None
+        best_score = 0
+        for mat_x in matches:
+            self.logger.debug(f"Start: {mat_x}")
+            route, route_score = self.select_route(mat_x, matches)
+            self.logger.debug(f"route: {route}")
+            self.logger.debug(f"route_score: {route_score}")
+            if route_score < best_score:
+                best_route = route
+                best_score = route_score
+
+        return best_route, best_score
+
+    def select_route(self, start_match, matches):
+
+        def score_func(match_x):
+            # use length as match score
+            return match_x[1]
+
+        buffer = matches.copy()
+        route = [start_match]
+        route_score = score_func(start_match)        
+
+        while buffer:
+            
+            current = route[-1]
+            current_score_max = 0
+            best_match = None
+            for match_x in buffer:                                 
+                cur_end = current[0] + current[1]
+                if match_x == current:                
+                    buffer.remove(match_x)
+                    continue
+                elif match_x[0] < cur_end:
+                    buffer.remove(match_x)
+                    continue
+                elif match_x[0] >= cur_end:                    
+                    cur_score = score_func(match_x)                    
+                    if current_score_max < cur_score:
+                        self.logger.debug(f"Route(score): {match_x}({cur_score})")
+                        best_match = match_x
+                        current_score_max = cur_score
+
+            if best_match:
+                buffer.remove(best_match)
+                route.append(best_match)
+                route_score += current_score_max                
+
+        return route, route_score
+
     def segment(self, text):
-        candids = self.lexicon.find_prefixes(text)
+        candids = self.lexicon.find_prefixes(text)        
         matches = []
         annot_data = {}
         for pat_x in candids:
--- test/test_segment.py
+++ test/test_segment.py
@@ -10,3 +10,30 @@ def test_segment():
     assert segs == ['我', '不知道', '你', '要', '不知道', '的', '知道', '!!!!']
     assert anno_data == {'不知道': {}, '!+': {'form': '3.2'}}
 
+def test_segment_conflict():
+    lex = Lexicon()    
+    lex.add_word("知不知道", True, {"form": "3.2"})
+    lex.add_word("不知道的知道", True, {"form": "3.2"})
+    lex.add_word("!+", True, {"form": "3.2"})
+    seg = Segmenter(lex)
+    segs, anno_data = seg.segment("我不知道你知不知道的知道!!!!")    
+    print(segs, anno_data)
+    assert segs == ['我', '不', '知道', "你", "知", "不知道的知道", '!!!!']
+    assert False
+
+def test_segment_conflict2():
+    lex = Lexicon()    
+    lex.add_word("不知道", True, {"form": "3.2"})
+    lex.add_word("不知道的知道", True, {"form": "3.2"})
+    lex.add_word("!+", True, {"form": "3.2"})
+    seg = Segmenter(lex)
+    segs, anno_data = seg.segment("我不知道你不知道的知道!!!!")    
+    print(segs, anno_data)
+    assert segs == ['我', '不知道', "你", "不知道的知道", '!!!!']    
+
+def test_segment_best_route():
+    lex = Lexicon()    
+    seg = Segmenter(lex)
+    route = seg.find_best_route([(0,2), (0,3), (2,5), (3,2)])
+    print(route)
+    assert route == [(0,2), (2,5)] 
